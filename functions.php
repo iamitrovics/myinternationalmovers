d80421874973cc0a85b78d384bf1ee9a8c2e2eb5
Srdjan Mitrovic <iam@mitrovics.com>
2022-05-16 09:06:40 +0200

    remove search

--- functions.php
+++ functions.php
@@ -136,3 +136,20 @@ function skip_mail_when_testing($f){
     return $handler->getIP() == '206.189.212.83'; //testing Bot IP address
 }
 add_filter('wpcf7_skip_mail','skip_mail_when_testing');
+
+
+
+function fb_filter_query( $query, $error = true ) {
+
+    if ( is_search() ) {
+        $query->is_search = false;
+        $query->query_vars['s'] = false;
+        $query->query['s'] = false;
+
+        if ( $error == true )
+            $query->is_404 = true;
+    }
+}
+
+add_action( 'parse_query', 'fb_filter_query' );
+add_filter( 'get_search_form', function() { return null;} );
