72139cc68cf8491847f3813988ac253ffe98b2bd
Srdjan Mitrovic <iam@mitrovics.com>
2022-07-18 21:28:45 +0200

    author removal plugin

--- functions.php
+++ functions.php
@@ -156,3 +156,38 @@ if ( ! is_admin() ) {
 	add_filter( 'get_search_form', function() { return null;} );
 
 }
+
+if (current_user_can('manage_options')) {
+	function lwp_2629_user_edit_ob_start() {ob_start();}
+	add_action( 'personal_options', 'lwp_2629_user_edit_ob_start' );
+	function lwp_2629_insert_nicename_input( $user ) {
+		$content = ob_get_clean();
+		$regex = '/<tr(.*)class="(.*)\buser-user-login-wrap\b(.*)"(.*)>([\s\S]*?)<\/tr>/';
+		$nicename_row = sprintf(
+			'<tr class="user-user-nicename-wrap"><th><label for="user_nicename">%1$s</label></th><td><input type="text" name="user_nicename" id="user_nicename" value="%2$s" class="regular-text" />' . "\n" . '<span class="description">%3$s</span></td></tr>',
+			esc_html__( 'Nicename' ),
+			esc_attr( $user->user_nicename ),
+			esc_html__( 'Must be unique.' )
+		);
+		echo preg_replace( $regex, '\0' . $nicename_row, $content );
+	}
+	add_action( 'show_user_profile', 'lwp_2629_insert_nicename_input' );
+	add_action( 'edit_user_profile', 'lwp_2629_insert_nicename_input' );
+	function lwp_2629_profile_update( $errors, $update, $user ) {
+		if ( !$update ) return;
+		if ( empty( $_POST['user_nicename'] ) ) {
+			$errors->add(
+				'empty_nicename',
+				sprintf(
+					'<strong>%1$s</strong>: %2$s',
+					esc_html__( 'Error' ),
+					esc_html__( 'Please enter a Nicename.' )
+				),
+				array( 'form-field' => 'user_nicename' )
+			);
+		} else {
+			$user->user_nicename = $_POST['user_nicename'];
+		}
+	}
+	add_action( 'user_profile_update_errors', 'lwp_2629_profile_update', 10, 3 );
+	}
